36628cf21389bf37661bd6b2deb494f424278da0
Mafia19902 <alessandro.cadeddu09@gmail.com>
2026-04-08 17:58:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -64,3 +64,30 @@ client.on('interactionCreate', async (interaction) => {
 });
 
 client.login(TOKEN);
+
+
+const { REST, Routes, SlashCommandBuilder } = require('index.js');
+
+const CLIENT_ID = "1491448051280121938";
+const GUILD_ID = "1463988119878766594"; // dein Server
+
+// Commands definieren
+const commands = [
+    new SlashCommandBuilder().setName('open').setDescription('Whitelist öffnen'),
+    new SlashCommandBuilder().setName('close').setDescription('Whitelist schließen')
+].map(cmd => cmd.toJSON()); // <- wichtig!
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+    try {
+        console.log('Slash-Commands werden registriert...');
+        await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            { body: commands }
+        );
+        console.log('Commands erfolgreich registriert ✅');
+    } catch (err) {
+        console.error(err);
+    }
+})();
